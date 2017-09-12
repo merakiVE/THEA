745bd9892db97c07b882bef7ae2dee69c8b1405e
Victor Pino <victopin0@gmail.com>
2017-09-12 16:49:47 -0400

    feat(components): Add Service Component

---
+++ src/components/Service/Service.js
@@ -0,0 +1,170 @@
+import React, {Component} from 'react';
+import { Form, Input, Icon, Button, Select } from 'antd';
+import './Service.css'
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class Service extends Component {
+    state = {
+        dataBase: [],
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+
+				let data = {
+				  method: 'POST',
+				  headers: {
+				    'Accept':       'application/json',
+				    'Content-Type': 'application/json',
+				  },
+				  body: JSON.stringify(values),
+				 
+				}
+				
+				console.log('Received values of form: ', values);
+
+				return fetch('http://localhost:8080/syndesi/connect', data)
+				       .then((response) => {
+					        return response.json()
+					    })
+						.then((data) => {
+							this.setState({ dataBase: data })
+							console.log(this.dataBase)
+						}) 
+            }
+        });
+    }
+
+    render() {
+        
+        const { getFieldDecorator, getFieldsError } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 6},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 14},
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 14,
+                    offset: 6,
+                },
+            },
+        };
+
+        return (
+            <div className="App">
+                <br/>
+                <Form style={{width: '60%', margin: '0 auto'}} onSubmit={this.handleSubmit}>
+                    
+                    <h3 className="titleForm"> Credenciales de la Base de Datos </h3>
+                    
+                    <br/>
+                    
+                    <FormItem
+                        {...formItemLayout}
+                        label="Host"
+                        hasFeedback>
+
+                        {getFieldDecorator('host', {
+                            rules: [{
+                                required: true, message: 'Por favor, digite el nombre del host',
+                            }],
+                        })(
+                        	<Input prefix={<Icon type="hdd" style={{ fontSize: 13 }} />} placeholder="IP" />
+                        )}
+                    </FormItem>
+
+                    <FormItem
+                        {...formItemLayout}
+                        label="Usuario"
+                        hasFeedback
+                    >
+                        {getFieldDecorator('user', {
+                            rules: [{
+                                required: true, message: 'Por favor, digite el nombre de usuario',
+                            }],
+                        })(
+                        	<Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Nombre de Usuario" />
+                        )}
+                    </FormItem>
+
+                    <FormItem
+                        {...formItemLayout}
+                        label="Password"
+                        hasFeedback
+                    >
+                        {getFieldDecorator('password', {
+                            rules: [{
+                                required: true, message: 'Por favor, digite la contrasena del usuario',
+                            },
+                                {
+                                    validator: this.checkConfirm,
+                                }],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="contrasena" />
+                        )}
+                    </FormItem>
+
+                    <FormItem
+                        {...formItemLayout}
+                        label="Tipo de BD"
+                        hasFeedback
+                    >
+                        {getFieldDecorator('typeBD', {
+				            rules: [{ required: true, message: 'Por favor, seleccione el tipo de base de datos' }],
+				        })(
+				            <Select placeholder="Selecciona el tipo">
+				              <Option value="postgresql">Postgresql</Option>
+				              <Option value="mysql">Mysql</Option>
+				              <Option value="oracle">Oracle</Option>
+				              <Option value="sqlite">Sqlite</Option>
+				            </Select>
+				        )}
+                    </FormItem>
+
+                    <FormItem
+                        {...formItemLayout}
+                        label="Nombre BD"
+                        hasFeedback
+                    >
+                        {getFieldDecorator('baseName', {
+                            rules: [{
+                                required: true, message: 'Por favor, digite el nombre de de la base de datos',
+                            }],
+                        })(
+                        	<Input prefix={<Icon type="edit" style={{ fontSize: 13 }} />} placeholder="Nombre de la base de datos" />
+                        )}
+                    </FormItem>
+
+
+                    <FormItem {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>Iniciar</Button>
+                    </FormItem>
+                </Form>
+
+            </div>
+        );
+    }
+}
+
+const ServiceForm = Form.create()(Service);
+export default ServiceForm;
